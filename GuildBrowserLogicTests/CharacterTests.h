026e1d91934ee96c026db6e3c89ba71abf10af58
Pierre-Jean Quilleré <pj.quillere@sylights.com>
2013-02-27 14:56:50 +0100

    Add test suit for Character
    With some not compiling, for testing purpose :]
    
    Signed-off-by: Pierre-Jean Quilleré <pj.quillere@sylights.com>

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Pierre-Jean Quillere on 27/02/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
